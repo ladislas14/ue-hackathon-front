d8b47c35c8a5c58117614d9199e296a1ddf33b1c
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-08 17:07:22 +0100

    minor tweak

--- src/components/cards/Tree.tsx
+++ src/components/cards/Tree.tsx
@@ -13,7 +13,7 @@ class Tree extends React.Component<TreeProps> {
             <View style={{backgroundColor: "#eee", borderRadius: 10, padding: 20, margin: 20}}>
                 {plant && (
                     <>
-                        {plant.image_url && <Image source={{uri: plant.image_url}} />}
+                        {plant.image_url && <Image source={{uri: plant.image_url}} style={{width: 100}} />}
                         <Text>{plant.id}</Text>
                         <Text>{plant.scientific_name}</Text>
                         {plant.common_name && plant.common_name.length > 0 && <Text>{plant.common_name}</Text>}
