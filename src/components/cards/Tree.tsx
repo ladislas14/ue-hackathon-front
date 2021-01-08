642cef061fe92a1e2c9033e76f7a771fbb3b15e2
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-08 16:44:13 +0100

    improvements

--- src/components/cards/Tree.tsx
+++ src/components/cards/Tree.tsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import{StyleSheet, View, Text, Image} from 'react-native'
-import TreflePlantDto from '../api/trefle/dto.ts'
+import React from "react";
+import {View, Text, Image} from "react-native";
+import {TreflePlantDto} from "../../api/trefle/dto";
 
 export type TreeProps = {
-    specie : TreflePlantDto | null
-}
+    plant: TreflePlantDto | null;
+};
 
-class Tree extends React.Component<TreeProps>
-{
-    render(): JSX.Element
-    {
-        return(
+class Tree extends React.Component<TreeProps> {
+    render(): JSX.Element {
+        const {plant} = this.props;
+        return (
             <View>
-                <Image source={{this.props.specie.image_url}}/>
-                <Text> this.props.specie.id</Text>
-                <Text> this.props.specie.common_name</Text>
-                <Text> this.props.specie.slug</Text>
-                <Text> this.props.specie.scientific_name</Text>
-                <Text> this.props.specie.id</Text>
-                <Text> this.props.specie.id</Text>
-                <Text> this.props.specie.id</Text>
-
-                            
+                {plant && (
+                    <>
+                        {plant.image_url && <Image source={{uri: plant.image_url}} />}
+                        <Text>{plant.id}</Text>
+                        <Text>{plant.slug}</Text>
+                        <Text>{plant.scientific_name}</Text>
+                    </>
+                )}
             </View>
-        )
+        );
     }
 }
 
-export default Tree
+export default Tree;
